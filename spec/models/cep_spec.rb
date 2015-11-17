8821543dd130e9efe100731a2ad2f21fcf46f978
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-17 11:06:47 -0200

    Adding tests to association between CEP and Supermarket.

--- spec/models/cep_spec.rb
+++ spec/models/cep_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Cep, type: :model do
     it { should validate_presence_of(:code) }
     it { should validate_uniqueness_of(:code) }
   end
+
+  describe 'assotiations' do
+    it { should have_many(:supermarkets) }
+  end
+
 end
--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Supermarket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:supermarket) { Supermarket.new}
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:cep_id) }
+  end
+
+  describe 'assotiations' do
+    it { should belongs_to(:cep) }
+  end
+
 end
