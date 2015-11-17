a5ccaa20f2a298b59433fa39199d309b36c4466b
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-17 15:11:21 -0200

    Creating presence validations. Creating association between supermarket and product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :supermarket
+
+  validates :name, :price, :supermarket_id, presence: true
 end
--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,4 +1,5 @@
 class Supermarket < ActiveRecord::Base
+  has_many :products
   belongs_to :cep
   validates :name, :cep_id, presence: true
 end
