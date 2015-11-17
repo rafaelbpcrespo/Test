2bf9d1de0e51bf446254189a1d235bfd18f0359a
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-17 15:12:03 -0200

    Creating spec for Product's model.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:product) { Product.new}
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:supermarket_id) }
+  end
+
+  describe 'assotiations' do
+    it { should belong_to(:supermarket) }
+  end
+
 end
