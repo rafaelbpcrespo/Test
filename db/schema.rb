cc22b81c3f5984e7c5f8d1d6ca73d083906251fb
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-17 15:11:54 -0200

    Schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117125916) do
+ActiveRecord::Schema.define(version: 20151117170536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20151117125916) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
+    t.string   "description"
+    t.integer  "supermarket_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "products", ["supermarket_id"], name: "index_products_on_supermarket_id", using: :btree
 
   create_table "supermarkets", force: :cascade do |t|
     t.string   "name"
@@ -49,5 +60,6 @@ ActiveRecord::Schema.define(version: 20151117125916) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "products", "supermarkets"
   add_foreign_key "supermarkets", "ceps"
 end
