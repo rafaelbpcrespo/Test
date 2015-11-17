5bbbd7a65950a69d566085faad2d4fa5e8073874
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-17 15:32:57 -0200

    Adding UUID attribute on User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117170536) do
+ActiveRecord::Schema.define(version: 20151117173149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20151117170536) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "uuid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
