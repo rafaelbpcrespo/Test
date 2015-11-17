31d9b94da73e8d5cb9193481dfbc27b2fb1487dd
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-17 10:53:14 -0200

    Created tests of validations to CEP.

--- spec/models/cep_spec.rb
+++ spec/models/cep_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Cep, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cep) { Cep.new}
+
+  describe 'validations' do
+    it { should validate_presence_of(:code) }
+    it { should validate_uniqueness_of(:code) }
+  end
 end
