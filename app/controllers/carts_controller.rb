301b0954cbcfbb0d959699564fc0b13854d9eaea
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-18 11:52:43 -0200

    Created method to checkout the order and finalize the cart.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,13 @@
 class CartsController < ApplicationController
   def show
-    @items = current_order.order_items
+    @items = current_order.items
+  end
+
+  def checkout
+    @order = current_order
+    @order.checkout
+    respond_to do |format|
+      format.html { redirect_to home_index_path, notice: 'Pedido realizado com sucesso.' }
+    end
   end
 end
