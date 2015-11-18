239d29a08c481442b860571477f88f7b2f45cf08
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-18 15:07:20 -0200

    Creating products search form.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,20 @@ class Product < ActiveRecord::Base
   has_many :orders, through: :items
 
   validates :name, :price, :supermarket_id, presence: true
+  before_create :titleize_name
+
+  def titleize_name
+    self.name = self.name.titleize
+    self.save!
+  end
+
+  def self.search(search)
+    if search
+      search = search.titleize
+      find(:all, :conditions => ['nome LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
+
 end
