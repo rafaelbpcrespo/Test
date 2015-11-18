682cb05726af3ba4cad23b1979db3ab7e4ed8ca3
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-18 12:19:13 -0200

    Adding association with orders. Created method to show if user has or not some order in progress.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :orders
+
+  def order_in_progress
+    self.orders.where(uuid: self.uuid).first
+  end
 end
