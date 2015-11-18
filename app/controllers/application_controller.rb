1f2b168ae6df77b4e875fcc06edbc06429f23b7c
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-18 11:47:21 -0200

    Fixing current_order method to return the order in progress if the user has some cart opened.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_order
 
   def current_order
-    if current_user.order_in_progress.nil?
-      Order.find(current_user.order_in_progress.id)
-    else
-      Order.new
+    if user_signed_in?
+      if !current_user.order_in_progress.nil?
+        return Order.find(current_user.order_in_progress.id)
+      else
+        return Order.new(uuid: current_user.uuid, user_id: current_user.id)
+      end
     end
   end
 end
