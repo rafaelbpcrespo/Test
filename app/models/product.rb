411bcb479dffa663fb514ce72d92de3dfc3b9972
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-18 12:18:44 -0200

    Adding association with order through items.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :supermarket
   has_many :items
+  has_many :orders, through: :items
 
   validates :name, :price, :supermarket_id, presence: true
 end
