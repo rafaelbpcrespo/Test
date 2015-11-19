4f11c3f7ad3b0f88df755b5bdea5fa2557c8dd08
rafaelbpcrespo <rafaelbpcrespo@gmail.com>
2015-11-19 14:10:32 -0200

    Set before_save to set total of order.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,16 @@ class Order < ActiveRecord::Base
   has_many :products, through: :items
   belongs_to :user
   belongs_to :supermarket
+  before_save :set_total
 
   validates :user_id, :supermarket_id, presence: true
 
   def subtotal
     items.collect { |item| item.valid? ? (item.quantity * item.unit_price) : 0 }.sum
+  end
+
+  def set_total
+    self.total = self.subtotal
   end
 
   def checkout
